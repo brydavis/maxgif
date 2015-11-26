3cb37c184c16a75de6094fc7fd74c9551aa01cf2
brydavis <bryan@stax.io>
2015-11-25 23:56:46 -0800

    auto commit

--- resize/resize.go
+++ resize/resize.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/jpeg"
 	"io/ioutil"
@@ -11,51 +12,54 @@ import (
 func main() {
 	dir, _ := ioutil.ReadDir(".")
 	for _, item := range dir {
-		switch ext := path.Ext(item.Name()); ext {
-		case ".jpg", ".jpeg":
-			file, _ := os.Open(item.Name())
-			img, _ := jpeg.Decode(file)
+		// img := ResizePercent(item.Name(), 0.25)
+		img := ResizePixels(item.Name(), 100, 800)
+		out, _ := os.Create(fmt.Sprintf("resized_%s", item.Name()))
 
-			mx := img.Bounds().Max.X
-			my := img.Bounds().Max.Y
+		var opt jpeg.Options
+		opt.Quality = 1000
 
-			rect := image.Rect(0, 0, (mx/2)+1, (my/2)+1)
-			// rect := image.Rect(0, 0, mx, my)
+		jpeg.Encode(out, img, &opt)
+	}
+}
 
-			i := image.NewRGBA(rect)
+func ResizePercent(filename string, percent float64) (i *image.RGBA) {
+	switch ext := path.Ext(filename); ext {
+	case ".jpg", ".jpeg":
+		file, _ := os.Open(filename)
+		img, _ := jpeg.Decode(file)
 
-			// x, y := 0, 0
+		rect := image.Rect(0, 0, int(float64(img.Bounds().Max.X)*percent), int(float64(img.Bounds().Max.Y)*percent))
+		i = image.NewRGBA(rect)
+
+		for iy := 0; iy <= rect.Max.Y; iy += 1 {
 			for ix := 0; ix <= rect.Max.X; ix += 1 {
-				for iy := 0; iy <= rect.Max.Y; iy += 1 {
-					i.Set(ix, iy, img.At(ix, iy))
-					// i.Set(ix, iy, img.At(x, y))
-					// y += 4 // x += 2
-				}
-				// y += 2
-				// x += 4
-
+				i.Set(ix, iy, img.At(int(float64(ix)/percent), int(float64(iy)/percent)))
 			}
-
-			out, _ := os.Create("half_" + file.Name())
-
-			// var opt gif.Options
-			// opt.NumColors = 256
-
-			// g := new(gif.GIF)
-
-			// var palets []*image.Paletted
-			// palets = append(palets, i)
-
-			// g.Image = palets
-			// g.Delay = []int{100}
-			// g.LoopCount = 1000
-
-			// gif.EncodeAll(out, g)
-			var opt jpeg.Options
-			opt.Quality = 1000
-
-			jpeg.Encode(out, i, &opt)
-
 		}
 	}
+
+	return
+}
+
+func ResizePixels(filename string, x, y int) (i *image.RGBA) {
+
+	switch ext := path.Ext(filename); ext {
+	case ".jpg", ".jpeg":
+		file, _ := os.Open(filename)
+		img, _ := jpeg.Decode(file)
+
+		dx := float64(x) / float64(img.Bounds().Max.X)
+		dy := float64(y) / float64(img.Bounds().Max.Y)
+
+		rect := image.Rect(0, 0, x, y)
+		i = image.NewRGBA(rect)
+
+		for iy := 0; iy <= rect.Max.Y; iy += 1 {
+			for ix := 0; ix <= rect.Max.X; ix += 1 {
+				i.Set(ix, iy, img.At(int(float64(ix)/dx), int(float64(iy)/dy)))
+			}
+		}
+	}
+	return
 }
